a3dff1ebf4623222ea76fe91ff45e308b9b87665
Daniel Moreira <de8a88@gmail.com>
2018-12-11 17:34:32 +0000

    added more logic to the search bar on the search page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,27 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def search
+    # raise
+
+    @items = Item.where('? > credits', params[:max_credits])
+
+    # if params[:query].present?
+    #
+    #   @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    # else
+    #   @movies = Movie.all
+    # end
+
+    # credits = Item.where(params[:max_credits])
+    # address = Item.wher(params[:address])
+
+
+
+    # @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+    # @movies = Movie.where(title: params[:query])
+  end
+
   def show
     @item = Item.find(params[:id])
   end
