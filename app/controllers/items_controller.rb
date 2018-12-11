5286c99c329027d541f06f13b5d1951419961983
Daniel Moreira <de8a88@gmail.com>
2018-12-11 19:53:53 +0000

    Index page now filters by credits and location

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,32 +2,10 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    # @items = @items.near(params[:location]) if params[:location].present?
-  end
 
-  def search
-
-    # @items = Item.where('? > credits', params[:max_credits])
-
-    sql_query = "user.address ILIKE :address"
-    # @items = Item.where(sql_query, "%#{params[:address]}%")
-    @items = Item.near(params[:address])
-    raise
-
-    # if params[:query].present?
-    #
-    #   @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
-    # else
-    #   @movies = Movie.all
-    # end
-
-    # credits = Item.where(params[:max_credits])
-    # address = Item.wher(params[:address])
-
-
-
-    # @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
-    # @movies = Movie.where(title: params[:query])
+    # This will work with two queries
+    @items = @items.where('? > credits', params[:max_credits]) if params[:max_credits].present?
+    @items = @items.near(params[:address]) if params[:address].present?
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
-  get '/search', to: 'items#search'
 
   resources :dashboard, only: [:index]
   resources :bookings, only: [:index, :new, :create, :edit, :update] do
