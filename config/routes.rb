80b3a14c3ed14751ad1403b90a6afffd5d2e6b37
Daniel Moreira <de8a88@gmail.com>
2018-12-11 15:24:31 +0000

    Finished pushing cards info to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :show]
+
+  resources :items, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
   resources :dashboard, only: [:index]
   resources :bookings, only: [:index, :new, :create, :edit, :update] do
-    resources :reviews, only: [:new, :index, :create]
+    resources :reviews, only: [:index]
   end
 end
