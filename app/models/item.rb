a30400937e0ac74356647854c05d8fa3b65da1e3
Daniel Moreira <de8a88@gmail.com>
2018-12-11 18:38:27 +0000

    Added address to Items and removed address from Users

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,10 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :credits, presence: true
+
+  # geocoded_by :address
+
+  # def address
+  #   user.address
+  # end
 end
