73ac4c371380ad26dbbdd79c1fe30e918a01498f
Daniel Moreira <de8a88@gmail.com>
2018-12-12 18:03:14 +0000

    new booking logic implemented. booking model validations updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,12 @@
 class BookingsController < ApplicationController
-  def new
-    @booking = Booking.new
-  end
-
   def create
-    item = Item.find(item_)
-    @booking = Booking.find(params[:id])
+    item = Item.find(params[:item_id])
+    credits = item.credits
+    booking = Booking.new(booking_new_params)
+    booking.user = current_user
+    booking.item = item
+    booking.total_credits = credits
+    booking.save
     redirect_to item_path(item)
   end
 
@@ -19,5 +20,15 @@ class BookingsController < ApplicationController
 
   def booking_status_params
     params.permit(:status)
+  end
+
+  def booking_new_params
+    params.require(:booking).permit(
+      :request_message,
+      :start_date,
+      :end_date,
+      :item_id,
+      :total_credits
+    )
   end
 end
