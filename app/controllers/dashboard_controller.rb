ed436461fada3f15f85b2b2da0d867836ca9477c
Jonny Marshall <jonnymarshall5@gmail.com>
2018-12-13 11:40:05 +0000

    dashboard layout in place. booking cards started and item card description overflow fixed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
 
   get "/items-development", to: "front_end_development#index"
   get "/show-development", to: "front_end_development#show"
+  get "/dashboard-development", to: "front_end_development#dashboard"
 end
