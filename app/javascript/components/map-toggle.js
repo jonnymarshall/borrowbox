c30d501e0fa5d6ed8d845d9c0e1d8df5e13b9c96
Jonny Marshall <jonnymarshall5@gmail.com>
2018-12-16 15:05:38 +0000

    map toggle functionality built but map not displaying properly

---
+++ app/javascript/components/map-toggle.js
@@ -0,0 +1,31 @@
+console.log("hello from map toggle");
+
+const mapToggle = () => {
+  const showMapButton = document.getElementById('show-map-button');
+  const showListButton = document.getElementById('show-list-button');
+  const mapArea = document.getElementById('map-area');
+  const itemsList = document.getElementById('items-list');
+
+  showListButton.classList.add("hidden");
+  // showMapButton.classList.add("hidden");
+  mapArea.classList.add("hidden");
+  // itemsList.classList.add("hidden");
+
+  showMapButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    showMapButton.classList.add("hidden");
+    itemsList.classList.add("hidden");
+    mapArea.classList.remove("hidden");
+    showListButton.classList.remove("hidden");
+  });
+
+  showListButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    showListButton.classList.add("hidden");
+    mapArea.classList.add("hidden");
+    showMapButton.classList.remove("hidden");
+    itemsList.classList.remove("hidden");
+  });
+}
+
+export { mapToggle };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,19 @@
 console.log('hello from application.js')
 import "bootstrap";
 
-import "../components/tabs";
+import { initTabs } from "../components/tabs";
 
 import { bindSweetAlertButtonDemo } from '../components/confirmation';
 bindSweetAlertButtonDemo();
 
+import { mapToggle } from'../components/map-toggle';
+
+const dashboardTabsTest = document.getElementById('dashboard-tabs');
+if (dashboardTabsTest) {
+  initTabs();
+}
+
+const mapTest = document.getElementById('map');
+if (mapTest) {
+  mapToggle();
+}
