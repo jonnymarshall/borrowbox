9eb7f625f74c00c9af12e2d373f7196f328c6009
Daniel Moreira <de8a88@gmail.com>
2018-12-17 04:36:21 +0000

    refactored combined rating calc into the model instead of controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,40 +24,12 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @booking = Booking.new
-    ratings_array = [@item.user.rating, @item.rating]
-    @combined_rating = calculate_average(ratings_array)
+    @combined_rating = @item.combined_rating
     # Mapbox
     @markers = markers([@item])
   end
 
   private
-
-  def calculate_average(ratings_array)
-    if both_ratings_nil?
-      nil
-    elsif any_rating_nil?
-      average_with_nils(ratings_array)
-    else
-      average(ratings_array)
-    end
-  end
-
-  def both_ratings_nil?
-    @item.user.rating.nil? && @item.rating.nil?
-  end
-
-  def any_rating_nil?
-    @item.user.rating.nil? || @item.rating.nil?
-  end
-
-  def average(ratings_array)
-    ratings_array.inject(0) { |sum, x| sum + x.to_f } / ratings_array.size
-  end
-
-  def average_with_nils(ratings_array)
-    clean_ratings = ratings_array.compact
-    average(clean_ratings)
-  end
 
   def sql_query
     query = []
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,38 @@ class Item < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   mount_uploader :photo, PhotoUploader
+
+  def combined_rating
+    ratings_array = [self.user.rating, self.rating]
+    calculate_average(ratings_array).round(1)
+  end
+
+  private
+
+  def calculate_average(ratings_array)
+    if both_ratings_nil?
+      nil
+    elsif any_rating_nil?
+      average_with_nils(ratings_array)
+    else
+      average(ratings_array)
+    end
+  end
+
+  def both_ratings_nil?
+    self.user.rating.nil? && self.rating.nil?
+  end
+
+  def any_rating_nil?
+    self.user.rating.nil? || self.rating.nil?
+  end
+
+  def average(ratings_array)
+    ratings_array.inject(0) { |sum, x| sum + x.to_f } / ratings_array.size
+  end
+
+  def average_with_nils(ratings_array)
+    clean_ratings = ratings_array.compact
+    average(clean_ratings)
+  end
 end
