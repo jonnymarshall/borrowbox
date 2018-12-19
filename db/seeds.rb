b33b42f599567eade0a79af6e5336670ca39a8af
Daniel Moreira <de8a88@gmail.com>
2018-12-19 15:07:03 +0000

    refactored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -200,8 +200,8 @@ puts "Created #{User.count} users..."
 #----------ITEM SEEDS----------
 puts 'Creating items...'
 
-path = "pinned-places.json"
-json_serialized = open(path).read
+pinned_places = "db/auxilliary/pinned-places.json"
+json_serialized = open(pinned_places).read
 json = JSON.parse(json_serialized)
 coordinates = []
 json["features"].each do |feature|
@@ -714,103 +714,12 @@ puts "Created #{Booking.count} bookings..."
 #----------REVIEWS SEEDS----------
 puts 'Creating reviews...'
 
-reviews = [
-  {
-    title: "Good as new",
-    content: "The item was impeccable, exactly what I needed in perfect conditions. The lender was very nice and helping!",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Very happy",
-    content: "It was my first time using the service and I'm very happy with how easy and casual the whole thing was, the lender was very nice and helpfull, definitly recommend it to everyone.",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Not satisfied",
-    content: "The item didn't correspond my expectations, the lender wasn't honest about it's conditions, I will look for another lender next time.",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Helpfull!",
-    content: "The lender was flexible around my difficult schedule, very quick with the messages and the item was exactly what I needed.",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "So friendly",
-    content: "It's nice how easy it is to get what you need, just like asking for a friend! Both the lender and the item were great!",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Good",
-    content: "Standard, easy to communicate and arrange the pick up, item worked fine and the lender was a good person",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Overall a good experience",
-    content: "The lender was quick at responding to the messages and the item worked fine",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "100% Recommend",
-    content: "Happy I didn't have to spend much money at the shop for a one time thing, it was easy to arrange and very convenient",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Great!",
-    content: "Will be telling friends and family about the experience, the lender and the item were great",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Very good",
-    content: "The item performed as expected, the lender was very easy and helpful",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Awesome",
-    content: "So easy to get everything done and definitly way cheaper",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Easy and Familiar",
-    content: "The whole process was really easy and familiar, just like asking a mate",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Convenience at it's peak",
-    content: "Will definitly start browsing here for most things, the item was in perfect condition and the lender was awesome too",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Okay",
-    content: "It was hard to arrange the meeting and work around the lender's schedule but the item was good",
-    item_rating: rand(3..5)
-  },
-
-  {
-    title: "Easy",
-    content: "Uncomplicated and advantageous!",
-    item_rating: rand(3..5)
-  },
-]
-    # byebug
+require_relative "auxilliary/reviews"
 
 n_reviews = n_bookings * 3
 
 n_reviews.times do
-  attribute = reviews.sample
+  attribute = @reviews.sample
   review = Review.new(
     title: attribute[:title],
     content: attribute[:content],
