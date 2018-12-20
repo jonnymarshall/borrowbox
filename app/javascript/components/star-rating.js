0574f5f99b91a4684c31ec5bcde7bb93144a7657
Daniel Moreira <de8a88@gmail.com>
2018-12-20 19:12:38 +0000

    stars now working as a function

--- app/javascript/components/star-rating.js
+++ app/javascript/components/star-rating.js
@@ -1,6 +1,10 @@
-document.getElementById('clear-button').addEventListener('click', function () {
-  ["min_rating_5", "min_rating_4", "min_rating_3", "min_rating_2", "min_rating_1"].forEach(function(id) {
-    document.getElementById(id).checked = false;
-  });
-  return false;
-})
+const clearStars = () => {
+  document.getElementById('clear-button').addEventListener('click', function () {
+    ["min_rating_5", "min_rating_4", "min_rating_3", "min_rating_2", "min_rating_1"].forEach(function(id) {
+      document.getElementById(id).checked = false;
+    });
+    return false;
+  })
+}
+
+export { clearStars }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,23 +3,21 @@ import "bootstrap";
 import './application.css';
 
 import { initTabs } from "../components/tabs";
-
 import { bindSweetAlertButtonDemo } from '../components/confirmation';
-bindSweetAlertButtonDemo();
+import { responseMessageNotification } from'../components/response-message-notification';
+import { dateSelector } from '../components/dateselector';
+import { clearStars } from '../components/star-rating';
+
 
 const dashboardTabsTest = document.getElementById('dashboard-tabs');
 if (dashboardTabsTest) {
   initTabs();
 }
 
-import { responseMessageNotification } from'../components/response-message-notification';
-
 if (document.getElementById('response-message-notification')) {
   responseMessageNotification();
 }
 
-import { dateSelector } from '../components/dateselector';
-
+bindSweetAlertButtonDemo();
 dateSelector();
-
-import '../components/star-rating';
+clearStars();
