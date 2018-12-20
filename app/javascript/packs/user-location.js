46853befa56c6a53c2b343ae2d7da346557445fc
Daniel Moreira <de8a88@gmail.com>
2018-12-20 14:45:41 +0000

    distance now working

--- app/javascript/packs/user-location.js
+++ app/javascript/packs/user-location.js
@@ -1,5 +1,5 @@
 window.onload = getLocation();
-console.log("hello from the user-location");
+// console.log("hello from the user-location");
 var x = document.getElementById("coordinates");
 function getLocation() {
   if (navigator.geolocation) {
